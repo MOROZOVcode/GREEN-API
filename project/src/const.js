c662b3ac5e5676069e1ab03c4b2a046f3ceede8c
MOROZOVcode <morozov.code@gmail.com>
2023-06-14 11:26:15 +0400

    Добавит маршруты для API

--- project/src/const.js
+++ project/src/const.js
@@ -2,3 +2,12 @@ export const NameSpace = {
   chat: 'CHAT',
   user: 'USER',
 };
+
+export const BACKEND_URL = 'https://api.green-api.com/waInstance';
+export const REQUEST_TIMEOUT = 5000;
+
+export const APIRoute = {
+  receiveNot: '/receiveNotification',
+  deleteNot: '/deleteNotification',
+  sendMessage: '/sendMessage/',
+};
