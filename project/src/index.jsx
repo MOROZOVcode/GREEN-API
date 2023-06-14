9cb00e03e78fa80e44379de68e7c7a9a639964ea
MOROZOVcode <morozov.code@gmail.com>
2023-06-14 15:28:08 +0400

    Подключит react-toastify

--- project/src/index.jsx
+++ project/src/index.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './app/app';
 import { store } from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ToastContainer />
       <App />
     </Provider>
   </React.StrictMode>
