9fe913b6c9053a82199e9421473af54faa33c9f9
MOROZOVcode <morozov.code@gmail.com>
2023-06-14 14:37:44 +0400

    Bagfix

--- project/src/store/chat-data/chat-data.slice.js
+++ project/src/store/chat-data/chat-data.slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { dropMessageAction, fetchMessageAction } from '../api-action';
+import { NameSpace } from '../../const';
 
 const initialState = {
   receiptId: null,
@@ -8,12 +9,12 @@ const initialState = {
 };
 
 export const chatData = createSlice({
-  name: NameSpace.user,
+  name: NameSpace.chat,
   initialState,
   reducers: {},
   extraReducer(builder) {
     builder
-      .addCase(fetchMessageAction.fulfilled, (state) => {
+      .addCase(fetchMessageAction.fulfilled, (state, action) => {
         state.receiptId = action.payload.receiptId;
         state.idMessage = action.payload.body.idMessage;
         state.textMessage = action.payload.body.messageData.typeMessage;
--- project/src/store/root-reducer.js
+++ project/src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { NameSpace } from '../const';
 import { userData } from './user-data/user-data.slice';
+import { chatData } from './chat-data/chat-data.slice';
 
 export const rootReducer = combineReducers({
   [NameSpace.user]: userData.reducer,
+  [NameSpace.chat]: chatData.reducer,
 });
